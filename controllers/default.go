b955f7f7c2119f24ad60c8168a67c5e1cd28e195
benny.zhou <benny.zhou@evolveconsulting.com.hk>
2017-06-12 17:00:47 +0800

    wx by zc

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"github.com/TitanBenny/weixin"
+
 	"github.com/astaxie/beego"
 )
 
@@ -12,4 +14,14 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+type WXController struct {
+	beego.Controller
+}
+
+func (c *WXController) Get() {
+	r := c.Ctx.Request
+	w := c.Ctx.ResponseWriter.ResponseWriter
+	weixin.HandleAccess(w, r)
 }
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"expanse/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/wx", &controllers.WXController{})
 }
